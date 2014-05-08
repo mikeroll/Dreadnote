d195333b3e7f663c6f9edbe0e8e10e4791795df7
Mikhail Bulash <scaryspiderpig@gmail.com>
2014-05-08 20:35:04 +0300

    Link following support

--- Dreadnote/src/main/java/com/mikeroll/dreadnote/frontend/Preview.java
+++ Dreadnote/src/main/java/com/mikeroll/dreadnote/frontend/Preview.java
@@ -2,15 +2,25 @@ package com.mikeroll.dreadnote.frontend;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 import com.commonsware.cwac.anddown.AndDown;
 import com.mikeroll.dreadnote.R;
 
 public class Preview extends Fragment {
+
+    private WebView webView;
+
+    /** Needed to process links not starting with "http://" */
+    private static final String httpRedir = "http://re.dir/";
 
     public Preview() {}
 
@@ -24,7 +34,23 @@ public class Preview extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_preview, container, false);
-        if (v != null) v.findViewById(R.id.preview).setBackgroundColor(0);
+        if (v != null) {
+            v.findViewById(R.id.preview).setBackgroundColor(0);
+            webView = (WebView) v.findViewById(R.id.preview);
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    if (url.startsWith("note:")) {
+                        //TODO: well, implement.
+                        Toast.makeText(getActivity(), "Note linking not yet implemented.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        url = url.replace(httpRedir, "http://");
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                    }
+                    return true;
+                }
+            });
+        }
         return v;
     }
 
@@ -43,8 +69,7 @@ public class Preview extends Fragment {
         if (a != null) a.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                WebView webView = (WebView) getView().findViewById(R.id.preview);
-                webView.loadData(html, "text/html; charset=UTF-8", null);
+                webView.loadDataWithBaseURL(httpRedir, html, "text/html; charset=UTF-8", null, null);
             }
         });
     }
