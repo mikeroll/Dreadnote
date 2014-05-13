9bb89fcfab8b80006f29b6f52d6e2158ea4a4ace
Mikhail Bulash <scaryspiderpig@gmail.com>
2014-05-14 01:33:30 +0300

    Async db operations

--- Dreadnote/src/main/java/com/mikeroll/dreadnote/frontend/Dashboard.java
+++ Dreadnote/src/main/java/com/mikeroll/dreadnote/frontend/Dashboard.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.SparseBooleanArray;
@@ -27,14 +28,14 @@ public class Dashboard extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         performFirstRunConfig();
-
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_dashboard);
 
         noteList = (ListView) findViewById(R.id.note_list);
         noteListAdapter  = new ResourceCursorAdapter(this, R.layout.list_item, null, 0) {
             @Override
             public void bindView(View view, Context context, Cursor cursor) {
-                int id = cursor.getInt(0);
+                long id = cursor.getInt(0);
                 String title = cursor.getString(1);
                 int color = cursor.getInt(2);
                 ((TextView) view.findViewById(R.id.list_item_title)).setText(title);
@@ -59,16 +60,10 @@ public class Dashboard extends Activity {
         }
     }
 
-    private void updateList() {
-        Cursor c = mDBClient.selectAll(); //FIXME: This is very bad.
-        noteListAdapter.changeCursor(c);
-        noteListAdapter.notifyDataSetChanged();
-    }
-
     @Override
     protected void onResume() {
         super.onResume();
-        updateList();
+        new UpdateListTask().execute();
     }
 
     @Override
@@ -106,24 +101,54 @@ public class Dashboard extends Activity {
         startActivity(intent);
     }
 
+    @SuppressWarnings({ "unchecked", "ConstantConditions" })
     private void deleteSelected() {
         SparseBooleanArray selected = noteList.getCheckedItemPositions();
         if (selected != null) {
             ArrayList<Long> ids = new ArrayList<Long>();
             for (int i = 0; i < noteListAdapter.getCount(); i++) {
                 if (selected.get(i)) {
-                    ids.add(((Cursor) noteListAdapter.getItem(i)).getLong(0));
+                    ids.add((Long)noteList.getChildAt(i).getTag());
                 }
             }
-            mDBClient.deleteNotes(ids);
-            updateList();
+            new DeleteNotesTask().execute(ids);
+            new UpdateListTask().execute();
+        }
+    }
+
+    private class DeleteNotesTask extends AsyncTask<ArrayList<Long>, Void, Void> {
+
+        @Override
+        protected Void doInBackground(ArrayList<Long>... ids) {
+            mDBClient.deleteNotes(ids[0]);
+            return null;
+        }
+    }
+
+    private class UpdateListTask extends AsyncTask<Void, Void, Cursor> {
+
+        @Override
+        protected void onPreExecute() {
+            setProgressBarIndeterminateVisibility(true);
+        }
+
+        @Override
+        protected Cursor doInBackground(Void... voids) {
+            return mDBClient.selectAll();
+        }
+
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            noteListAdapter.changeCursor(cursor);
+            noteListAdapter.notifyDataSetChanged();
+            setProgressBarIndeterminateVisibility(false);
         }
     }
 
     private class OnNoteClickListener implements AdapterView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-            int id = (Integer)view.getTag();
+            long id = (Long)view.getTag();
             openNote(id);
         }
     }
